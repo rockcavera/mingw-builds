cb1a3398aff79d93b2bcc04893c7d43b47c4a3ed
rockcavera <rockcavera@gmail.com>
2023-01-04 20:45:12 -0300

    libiberty and jit

--- scripts/gcc-5.5.0.sh
+++ scripts/gcc-5.5.0.sh
@@ -62,6 +62,7 @@ PKG_PATCHES=(
 	gcc/gcc-5.2-fix-mingw-pch.patch
 	gcc/gcc-5-dwarf-regression.patch
 	gcc/gcc-5.1.0-fix-libatomic-building-for-threads=win32.patch
+  gcc/gcc-5.5.0-libiberty-and-jit.patch
 )
 
 #
